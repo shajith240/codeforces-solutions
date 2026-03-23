fedd0d659f528b2abdd4b4f79a1191f3ad455819
shajith <24je0598@iitism.ac.in>
2026-03-23 23:49:43 +0530

    [CF] A from contest 510 — AC

---
+++ 2026/02/09/fox-and-snake.cpp
@@ -0,0 +1,66 @@
+/*
+ * Problem: A. Fox And Snake
+ * Contest: 510/A | Link: https://codeforces.com/contest/510/problem/A
+ *
+ * ── Problem ──────────────────────────────────────────────
+ * Fox Ciel starts to learn programming. The first task is drawing a fox! However, that turns out to be too hard for a beginner, so she decides to draw a snake instead. A snake is a pattern on a n by m table. Denote c -th cell of r -th row as ( r , c ) . The tail of the snake is located at (1, 1) , then it's body extends to (1, m ) , then goes down 2 rows to (3, m ) , then goes left to (3, 1) and so on. Your task is to draw this snake for Fox Ciel: the empty cells should be represented as dot characters (' . ') and the snake cells should be filled with number signs (' # '). Consider sample tests in order to understand the snake pattern.
+ *
+ * ── Input ────────────────────────────────────────────────
+ * The only line contains two integers: n and m ( 3 ≤ n , m ≤ 50 ). n is an odd number.
+ *
+ * ── Output ───────────────────────────────────────────────
+ * Output n lines. Each line should contain a string consisting of m characters. Do not output spaces.
+ *
+ * ── Example Input ────────────────────────────────────────
+ * 3 3 | 3 4 | 5 3 | 9 9
+ *
+ * ── Example Output ───────────────────────────────────────
+ * ### ..# ### | #### ...# #### | ### ..# ### #.. ### | ######### ........# ######### #........ ######### ........# ######### #........ #########
+ *
+ */
+
+#include<bits/stdc++.h>
+using namespace std;
+
+
+void solve()
+{
+    int n , m ;
+    cin >> n >> m;
+    int oddCount = 1;
+    for(int i = 0 ; i < n ; i++){
+
+        if(i % 2 == 0){
+           
+            for(int j = 0 ; j < m ;j++){
+                cout << '#';
+            }
+            cout << endl;
+        }
+        else{
+            if(oddCount % 2 == 1){
+                for(int j = 0 ; j < m - 1 ; j++){
+                    cout << '.';
+                }
+
+                cout << "#";
+                cout << endl;
+                oddCount++;
+            }
+            else{
+                cout << '#';
+                for(int j = 0 ; j < m - 1 ; j++){
+                    cout << '.';
+                }
+                cout << endl;
+                oddCount++;
+            }
+        }
+
+        
+    }
+}
+int main(){
+    solve();
+    return 0;
+}
