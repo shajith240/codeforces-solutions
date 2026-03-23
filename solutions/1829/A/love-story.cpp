c029f260d2ed70bbed192e9de3dc543a372a1294
shajith <24je0598@iitism.ac.in>
2026-03-23 23:39:48 +0530

    [CF] A from contest 1829 — AC

---
+++ solutions/1829/A/love-story.cpp
@@ -0,0 +1,35 @@
+/*
+ * Problem: A. Love Story
+ * Contest: 1829 | Problem A
+ * Link: https://codeforces.com/contest/1829/problem/A
+ *
+ * --- Input ---
+ * The first line contains a single integer $$$t$$$ ($$$1 \leq t \leq 1000$$$) — the number of test cases. Each test case is one line and contains the string $$$s$$$, consisting of exactly $$$10$$$ lowercase Latin characters. Output For each test case, output a single integer — the number of indices where string $$$s$$$ differs. Example Input 5 coolforsez cadafurcie codeforces paiuforces forcescode Output 4 5 0 4 9
+ *
+ * --- Output ---
+ * For each test case, output a single integer — the number of indices where string $$$s$$$ differs. Example Input 5 coolforsez cadafurcie codeforces paiuforces forcescode Output 4 5 0 4 9
+ *
+ */
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int t ;
+    cin >> t;
+    while(t--){
+        string s;
+        string original = "codeforces";
+        cin >> s;
+
+        int count = 0;
+        for(int i = 0 ; i < s.length() ; i++){
+            if(s[i] != original[i]){
+                count++;
+            }
+        }
+
+        cout << count << endl;
+    }
+    return 0;
+}
