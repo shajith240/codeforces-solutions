0c8a5939437f2d414e66c929db12b9c5797a2fc7
shajith <24je0598@iitism.ac.in>
2026-05-23 13:42:21 +0530

    [LC] Lowest Common Ancestor of a Binary Search Tree — Accepted

---
+++ leetcode/2026/05/21/lowest-common-ancestor-of-a-binary-search-tree-2008764376.cpp
@@ -0,0 +1,67 @@
+/*
+ * Problem: Lowest Common Ancestor of a Binary Search Tree
+ * Difficulty: Medium
+ * Link: https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-search-tree/
+ * Language: cpp
+ * Topics: Tree, Depth-First Search, Binary Search Tree, Binary Tree
+ * 
+ * ── Problem ────────────────────────────────────────────
+ * Given a binary search tree (BST), find the lowest common ancestor (LCA) node of two given nodes in the BST.
+ * According to the definition of LCA on Wikipedia : &ldquo;The lowest common ancestor is defined between two nodes p and q as the lowest node in T that has both p and q as descendants (where we allow a node to be a descendant of itself ).&rdquo;
+ * Example 1:
+ * Input: root = [6,2,8,0,4,7,9,null,null,3,5], p = 2, q = 8
+ * Output: 6
+ * Explanation: The LCA of nodes 2 and 8 is 6.
+ * Example 2:
+ * Input: root = [6,2,8,0,4,7,9,null,null,3,5], p = 2, q = 4
+ * Output: 2
+ * Explanation: The LCA of nodes 2 and 4 is 2, since a node can be a descendant of itself according to the LCA definition.
+ * Example 3:
+ * Input: root = [2,1], p = 2, q = 1
+ * Output: 2
+ * Constraints:
+ * • The number of nodes in the tree is in the range [2, 10 5 ] .
+ * • -10 9 <= Node.val <= 10 9
+ * • All Node.val are unique .
+ * • p != q
+ * • p and q will exist in the BST.
+ * 
+ */
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+
+class Solution {
+public:
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        
+        if(root == NULL){
+            return NULL;
+        }
+
+        if(root == p || root == q){
+            return root;
+        }
+
+        TreeNode* leftN = lowestCommonAncestor(root->left,p,q);
+        TreeNode* rightN = lowestCommonAncestor(root->right,p,q);
+        
+
+        if(leftN != NULL && rightN != NULL){
+            return root;
+        }
+        
+        if(leftN != NULL){
+            return leftN;
+        }
+
+        return rightN;
+    }
+};
