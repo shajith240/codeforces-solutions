eba454de677b1ec3ccab13cf6422ca30793ab585
shajith <24je0598@iitism.ac.in>
2026-05-31 17:55:02 +0530

    [LC] Validate Binary Search Tree — Accepted

---
+++ leetcode/2026/05/31/validate-binary-search-tree.cpp
@@ -0,0 +1,55 @@
+/*
+ * Problem: Validate Binary Search Tree
+ * Difficulty: Medium
+ * Link: https://leetcode.com/problems/validate-binary-search-tree/
+ * Language: cpp
+ * Topics: Tree, Depth-First Search, Binary Search Tree, Binary Tree
+ * 
+ * ── Problem ────────────────────────────────────────────
+ * Given the root of a binary tree, determine if it is a valid binary search tree (BST) .
+ * A valid BST is defined as follows:
+ * • The left subtree of a node contains only nodes with keys strictly less than the node&#39;s key.
+ * • The right subtree of a node contains only nodes with keys strictly greater than the node&#39;s key.
+ * • Both the left and right subtrees must also be binary search trees.
+ * Example 1:
+ * Input: root = [2,1,3]
+ * Output: true
+ * Example 2:
+ * Input: root = [5,1,4,null,null,3,6]
+ * Output: false
+ * Explanation: The root node&#39;s value is 5 but its right child&#39;s value is 4.
+ * Constraints:
+ * • The number of nodes in the tree is in the range [1, 10 4 ] .
+ * • -2 31 <= Node.val <= 2 31 - 1
+ * 
+ */
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool dfs(TreeNode* Node,long minval,long maxval){
+       
+       if(Node == NULL){
+        return true;
+       }
+
+       if(Node->val <= minval || Node->val >= maxval){
+        return false;
+       }
+
+       return dfs(Node->left,minval,Node->val) && dfs(Node->right,Node->val,maxval);
+    }
+    bool isValidBST(TreeNode* root) {
+         return dfs(root,LONG_MIN,LONG_MAX);
+    }
+};
