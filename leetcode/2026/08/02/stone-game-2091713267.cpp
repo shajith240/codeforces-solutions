5ff32fe8969f41249e3a318288f20df4390cc1f8
shajith <24je0598@iitism.ac.in>
2026-08-02 23:17:10 +0530

    [LC] Stone Game — Accepted

---
+++ leetcode/2026/08/02/stone-game-2091713267.cpp
@@ -0,0 +1,67 @@
+/*
+ * Problem: Stone Game
+ * Difficulty: Medium
+ * Link: https://leetcode.com/problems/stone-game/
+ * Language: cpp
+ * Topics: Array, Math, Dynamic Programming, Game Theory
+ * 
+ * ── Problem ────────────────────────────────────────────
+ * Alice and Bob play a game with piles of stones. There are an even number of piles arranged in a row, and each pile has a positive integer number of stones piles[i] .
+ * The objective of the game is to end with the most stones. The total number of stones across all the piles is odd , so there are no ties.
+ * Alice and Bob take turns, with Alice starting first . Each turn, a player takes the entire pile of stones either from the beginning or from the end of the row. This continues until there are no more piles left, at which point the person with the most stones wins .
+ * Assuming Alice and Bob play optimally, return true if Alice wins the game, or false if Bob wins .
+ * Example 1:
+ * Input: piles = [5,3,4,5]
+ * Output: true
+ * Explanation:
+ * Alice starts first, and can only take the first 5 or the last 5.
+ * Say she takes the first 5, so that the row becomes [3, 4, 5].
+ * If Bob takes 3, then the board is [4, 5], and Alice takes 5 to win with 10 points.
+ * If Bob takes the last 5, then the board is [3, 4], and Alice takes 4 to win with 9 points.
+ * This demonstrated that taking the first 5 was a winning move for Alice, so we return true.
+ * Example 2:
+ * Input: piles = [3,7,2,3]
+ * Output: true
+ * Constraints:
+ * • 2 <= piles.length <= 500
+ * • piles.length is even .
+ * • 1 <= piles[i] <= 500
+ * • sum(piles[i]) is odd .
+ * 
+ */
+
+class Solution {
+public:
+    // int recursive_function(int l, int r, vector<int>& piles,vector<vector<int>>& dp) {
+
+    //     if (l == r) {
+    //         return piles[l];
+    //     }
+
+    //     if (dp[l][r] > 0) {
+    //         return dp[l][r];
+    //     }
+
+    //     int right = piles[l] - recursive_function(l + 1, r, piles,dp);
+    //     int left = piles[r] - recursive_function(l, r - 1, piles,dp);
+
+    //     int ans = max(left, right);
+
+    //     dp[l][r] = ans;
+
+    //     return ans;
+    // }
+
+    bool stoneGame(vector<int>& piles) {
+        // int n = piles.size();
+
+        // vector<vector<int>> dp(n,vector<int>(n,-1));
+
+        // int ans = recursive_function(0, n - 1, piles,dp);
+
+        // if (ans > 0) {
+        //     return true;
+        // }
+        return true;
+    }
+};
