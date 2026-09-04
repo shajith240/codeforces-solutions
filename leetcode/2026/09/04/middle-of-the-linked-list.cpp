60ed4489f1410f908fca7f84cb9e2f1c926853d9
shajith <24je0598@iitism.ac.in>
2026-09-04 18:24:44 +0530

    [LC] Middle of the Linked List — Accepted

---
+++ leetcode/2026/09/04/middle-of-the-linked-list.cpp
@@ -0,0 +1,53 @@
+/*
+ * Problem: Middle of the Linked List
+ * Difficulty: Easy
+ * Link: https://leetcode.com/problems/middle-of-the-linked-list/
+ * Language: cpp
+ * Topics: Linked List, Two Pointers
+ * 
+ * ── Problem ────────────────────────────────────────────
+ * Given the head of a singly linked list, return the middle node of the linked list .
+ * If there are two middle nodes, return the second middle node.
+ * Example 1:
+ * Input: head = [1,2,3,4,5]
+ * Output: [3,4,5]
+ * Explanation: The middle node of the list is node 3.
+ * Example 2:
+ * Input: head = [1,2,3,4,5,6]
+ * Output: [4,5,6]
+ * Explanation: Since the list has two middle nodes with values 3 and 4, we return the second one.
+ * Constraints:
+ * • The number of nodes in the list is in the range [1, 100] .
+ * • 1 <= Node.val <= 100
+ * 
+ */
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* middleNode(ListNode* head) {
+        
+        ListNode* temp = head;
+        int n = 0;
+        while(temp){
+            n++;
+            temp = temp->next;
+        }
+        
+        ListNode* ans = head;
+        for(int i = 0 ; i < n / 2 ; i++){
+            ans = ans->next;
+        }
+
+        return ans;
+    }
+};
